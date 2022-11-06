3242f34b3fff6ebe9bc7b6b3bdcdf08630b6a7ed
MD AL IMRAN <mdimrn99@gmail.com>
2022-11-07 03:39:55 +0600

    full todo app

---
+++ todo-app/backend/index.js
@@ -0,0 +1,28 @@
+import express from "express";
+import cors from "cors";
+import Routes from "./routes/route.js";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+const app = express();
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGODB_URL)
+  .then(() => console.log("Database is connected"))
+  .catch((err) => console.log(err));
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Welcome to my Todo App");
+});
+
+app.use("/", Routes);
+
+const PORT = 5000;
+
+app.listen(PORT, () =>
+  console.log(`Server is running successfully on PORT ${PORT}`)
+);
